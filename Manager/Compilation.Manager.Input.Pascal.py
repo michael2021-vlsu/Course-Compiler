cd0570198e7d33d07d907bd3f8256ec09f3b9218
Michael fS (16) <mikhail.alekseevich.lp@bk.ru>
2021-11-21 00:00:29 +0300

    manager is done

--- Manager/Compilation.Manager.Input.Pascal.py
+++ Manager/Compilation.Manager.Input.Pascal.py
@@ -20,18 +20,72 @@ while True:
     
     listOfTokens = serv.ProcessAsFunction(content={"session":session,"source":codePascal},
                            requested_function='Compilation.Lexer')
-    
-    #Lower is not ready now, wait for some... If it is very necessary, tell Michael.
-    abstractSyntaxTree = serv.ProcessAsFunction(content=listOfTokens,
-                           requested_function='Compilation.Parser')
 
-    codeAsm = serv.ProcessAsFunction(content=abstractSyntaxTree,
-                           requested_function='Compilation.CodeGenerator')
+    result = { "success":False, "lines":[] }
+
+    if "success" in listOfTokens:
+        if listOfTokens["success"]:
+            abstractSyntaxTree = serv.ProcessAsFunction(content={"session":session,"tokens":listOfTokens["tokens"]},
+                                   requested_function='Compilation.Parser')
+
+            if "success" in abstractSyntaxTree:
+                if abstractSyntaxTree["success"]:
+                    codeAsm = serv.ProcessAsFunction(content={"session":session,"variables":abstractSyntaxTree["variables"],"syntax tree":abstractSyntaxTree["syntax tree"]},
+                                   requested_function='Compilation.CodeGenerator')
+
+                    if "success" in codeAsm:
+                        if codeAsm["success"]:
+                            result["success"] = True
+                            result["lines"] = codeAsm["asm code"]
+                        else:
+                            first = None
+                            if "error desc" in codeAsm:
+                                first = ": Генератор кода: " + codeAsm["error desc"]
+                            else:
+                                first = ": Генератор кода: Описание ошибки отсутствует."
+                            
+                            result["lines"] = [ first ]
+                    else:
+                        result["lines"] = ["Сервис генератора кода неисправен!"]
+                else:
+                    first = None
+                    if "error desc" in abstractSyntaxTree:
+                        first = ": Парсер: " + abstractSyntaxTree["error desc"]
+                    else:
+                        first = ": Парсер: Описание ошибки отсутствует."
+                    
+                    if "error line" in abstractSyntaxTree:
+                        if "error column" in abstractSyntaxTree:
+                            first = "; символ: " + abstractSyntaxTree["error column"] + first
+                        first = "Строка " + abstractSyntaxTree["error line"] + first
+
+                        result["lines"] = [ first, codePascal[abstractSyntaxTree["error line"]] ]
+                    else:
+                        result["lines"] = [ first ]
+            else:
+                result["lines"] = ["Сервис парсера неисправен!"]
+        else:
+            first = None
+            if "error desc" in listOfTokens:
+                first = ": Лексер: " + listOfTokens["error desc"]
+            else:
+                first = ": Лексер: Описание ошибки отсутствует."
+            
+            if "error line" in listOfTokens:
+                if "error column" in listOfTokens:
+                    first = "; символ: " + listOfTokens["error column"] + first
+                first = "Строка " + listOfTokens["error line"] + first
+
+                result["lines"] = [ first, codePascal[listOfTokens["error line"]] ]
+            else:
+                result["lines"] = [ first ]
+    else:
+        result["lines"] = ["Сервис лексера неисправен!"]
 
     serv.ProcessAsFunction(content={"session":session,"bank":None,"operation":"post","data":"Manager's Mandat: Remove Session"},
                            requested_function='Compilation.Storage.Request')
     
     #End of data processing area
-    serv.PostFinalResult(content=codeAsm,
+    serv.PostFinalResult(content=result,
                          result_type='Compilation.Manager.Output',
                          responseAddress=addr)
