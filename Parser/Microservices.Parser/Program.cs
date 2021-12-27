f5a5786ddae3a297c0f7ec637c45e954a50db896
Michael fS (16) <mikhail.alekseevich.lp@bk.ru>
2021-12-27 05:05:23 +0300

    parser is almost completely ready

--- Parser/Microservices.Parser/Program.cs
+++ Parser/Microservices.Parser/Program.cs
@@ -1,3 +1,5 @@
+﻿// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 using Compilation.Parser.IOstructs;
 
 namespace Compilation.Parser {
--- Parser/Microservices.Parser/TokenMapping.cs
+++ Parser/Microservices.Parser/TokenMapping.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,86 +9,44 @@ using System.Text;
 namespace Compilation.Parser {
 
     static class TokenMapping {
-        public enum Meaning {
-            ProgramNameDefinition,
-            VariableAssigning
-        }
-
-        public static readonly Table[] Grammars = new Table[] {
-            new(Meaning.ProgramNameDefinition, new TableEntry[] { new(Modifer.Simple, new[] { Token.Program }),
-                                                      new(Modifer.Simple, new[] { Token.InternalName }),
-                                                      new(Modifer.Simple, new[] { Token.Semicolon })  }),
-
-            new(Meaning.VariableAssigning, new TableEntry[] { new(Modifer.Simple, new[] { Token.InternalName }),
-                                                      new(Modifer.Simple, new[] { Token.Assign }),
-                                                      new(Modifer.Repeateble, new[] { Token.Integer, Token.InternalName, Token.ArithmeticOperator, Token.ParenthesisLeft, Token.ParenthesisRight }),
-                                                      new(Modifer.Simple, new[] { Token.Semicolon })  })
-        };
-
-
-        public struct Table {
-            public Table(Meaning mean, TableEntry[] sequence) {
-                this.mean = mean;
-                this.sequence = sequence;
-            }
-
-            public Meaning mean;
-            public TableEntry[] sequence;
-        }
-
-        public struct TableEntry {
-            public TableEntry(Modifer modifiers, Token[] tokens) {
-                this.modifiers = modifiers;
-                this.tokens = tokens;
-            }
-
-            public Modifer modifiers;
-            public Token[] tokens;
-        }
-
-        public enum Modifer {
-            Simple, //Just value set
-            Nullable, //Allow to skip this set
-            Repeateble, //Allow to repeat multiple times
-            Not = 4 //Invert white-list set to a black-list set
-        }
 
         public enum Token { //"_" - means "flexible" category (need more accurate recognition)
-            Program, //program name definition keyword
-            Procedure, //Procedure definition start
-            Begin, End, //code section
-            Colon, //in this case, colon is keyword
-            Dot, //End of program
-            Comma, //Argument list or var def
-            Semicolon, //End of phrase
+            Program, //a program name definition keyword
+            Procedure, //b Procedure definition start
+            Begin, //c code section
+            End, //d code section
+            Colon, //e in this case, colon is keyword
+            Dot, //f End of program
+            Comma, //g Argument list or var def
+            Semicolon, //h End of phrase
 
-            Var, //Varables definition or pointer
-            IntegerDef, //DataType name: Integer
-            Const, //Const var
+            Var, //i Varables definition or pointer
+            IntegerDef, //j DataType name: Integer
+            Const, //k Const var
 
-            Assign, //Assing value to variable
-            Exit, //Exit from the program
+            Assign, //l Assing value to variable
+            Exit, //m Exit from the program
 
             //DataTypes
-            InternalName, //Variable-valid sequence (variable, function-name, etc.)
-            Integer, //Integer value (like 89)
-            Boolean, //True or False values
+            InternalName, //n Variable-valid sequence (variable, function-name, etc.)
+            Integer, //o Integer value (like 89)
+            Boolean, //p True or False values
 
-            ParenthesisLeft, // (
-            ParenthesisRight, // )
+            ParenthesisLeft, //q (
+            ParenthesisRight, //r )
 
-            ArithmeticOperator, //+, -, *, /, etc.
-            LogicOperator, //and, or
-            ComparsionOperator, // >, <, =, <>, <=, >=
+            ArithmeticOperator, //s +, -, *, /, etc.
+            LogicOperator, //t and, or
+            ComparsionOperator, //u >, <, =, <>, <=, >=
 
-            LoopFor, //for
-            LoopFor_to, //1 to 9
-            LoopFor_downto, //9 downto 1
-            LoopWhile, //while
-            Loop_do //while <condition> do
+            LoopFor, //v for
+            LoopFor_to, //w 1 to 9
+            LoopFor_downto, //x 9 downto 1
+            LoopWhile, //y while
+            Loop_do //z while <condition> do
         }
 
-        //([a-zA-Z_]\w*)|([\d+*\-\/]+)|(\((?:[a-zA-Z_]\w*|[\d+*\-\/]|((?3)))+\))
+        //\((?:[a-z]\w*|[\d+*\-\/]|(?R))+\)
         public static readonly Dictionary<string, Token> TokenCvt = new(new KeyValuePair<string, Token>[] {
             new("program", Token.Program),
             new("procedure", Token.Procedure),
